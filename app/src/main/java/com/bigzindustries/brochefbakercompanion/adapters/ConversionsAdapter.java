39020bf0885bdb4b9d7891c5a6c1f3f94461c6cd
Zachary Foster <zfoster22@gmail.com>
2018-01-31 22:46:54 -0800

    simpler list style for conversions within a recipe

--- app/src/main/java/com/bigzindustries/brochefbakercompanion/adapters/ConversionsAdapter.java
+++ app/src/main/java/com/bigzindustries/brochefbakercompanion/adapters/ConversionsAdapter.java
@@ -6,17 +6,11 @@ import android.support.v4.widget.CursorAdapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
-import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.bigzindustries.brochefbakercompanion.R;
-import com.bigzindustries.brochefbakercompanion.conversion.ConversionController;
-
-import java.util.HashMap;
 
 public class ConversionsAdapter extends CursorAdapter {
-
-    HashMap<View, ConversionController> controllers = new HashMap<>();
 
     public ConversionsAdapter(Context context, Cursor cursor) {
         super(context, cursor, 0);
@@ -24,33 +18,28 @@ public class ConversionsAdapter extends CursorAdapter {
 
     @Override
     public View newView(Context context, Cursor cursor, ViewGroup parent) {
-        return LayoutInflater.from(context).inflate(R.layout.conversion_view, parent, false);
+        return LayoutInflater.from(context).inflate(R.layout.simple_conversion, parent, false);
     }
 
     @Override
     public void bindView(View view, Context context, Cursor cursor) {
-        ConversionController controller = controllers.get(view);
-        if (controller == null) {
-            controller = new ConversionController(context, view);
-            controllers.put(view, controller);
-        }
-
-        controller.setValues(
-            cursor.getString(cursor.getColumnIndex("ingredient")),
-            cursor.getString(cursor.getColumnIndex("fromUnit")),
-            cursor.getString(cursor.getColumnIndex("toUnit")),
-            cursor.getDouble(cursor.getColumnIndex("fromValue")),
-            cursor.getDouble(cursor.getColumnIndex("toValue"))
+        ((TextView)view.findViewById(R.id.ingredient_value)).setText(cursor.getString(cursor.getColumnIndex("ingredient")).toLowerCase());
+        ((TextView)view.findViewById(R.id.from_value)).setText(
+                getMeasurementString(cursor.getDouble(cursor.getColumnIndex("fromValue")),
+                        cursor.getString(cursor.getColumnIndex("fromUnit")))
         );
+        ((TextView)view.findViewById(R.id.to_value)).setText(
+                getMeasurementString(cursor.getDouble(cursor.getColumnIndex("toValue")),
+                        cursor.getString(cursor.getColumnIndex("toUnit")))
+        );
+    }
 
-        // Find fields to populate in inflated template
-//        Spinner ingredient = view.findViewById(R.id.ingredient_spinner);
-//        Spinner fromUnit = view.findViewById(R.id.from_unit_spinner);
-//        Spinner toUnit = view.findViewById(R.id.to_unit_spinner);
-//        EditText fromVal = view.findViewById(R.id.from_value);
-//        EditText toVal = view.findViewById(R.id.to_value);
+    private String getMeasurementString(double val, String unit) {
+        StringBuilder builder = new StringBuilder();
+        builder.append(String.format("%.2f", val))
+                .append(" ")
+                .append(unit.toLowerCase());
 
-        // Populate fields with extracted cursor properties
-//        ingredient.
+        return builder.toString();
     }
 }
